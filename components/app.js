9d24dda4504e6a5631e5e74496f3d65ef1d96d30
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-07-23 21:23:45 +0200

    debug file

--- components/app.js
+++ components/app.js
@@ -29,8 +29,7 @@ var App = React.createClass({
   render: function() {
     return (
         <div className={'app'}>
-            <ContactForm
-                {contact= contactForm}>
+             <ContactForm contact= {contactForm}>
             </ContactForm>
             <Contacts
                 {items= contacts}>
